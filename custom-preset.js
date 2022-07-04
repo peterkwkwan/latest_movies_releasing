146aef3460beb38c67e44616b8a6584f78d2e88f
Peter Kwan <peterkwkwan@gmail.com>
2022-07-04 11:33:29 +0800

    feat: tailwind css preset

---
+++ custom-preset.js
@@ -0,0 +1,40 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: {
+        main: "#5324ed",
+        light: "#bfb3e8",
+      },
+      secondary: {
+        main: "f51d65",
+        light: "#e6a8bc",
+      },
+      gray: {
+        darkest: "#1f2d3d",
+        dark: "#3c4858",
+        DEFAULT: "#c0ccda",
+        light: "#e0e6ed",
+        lightest: "#f9fafc",
+      },
+    },
+    fontFamily: {
+      sans: [
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Fira Sans",
+        "Droid Sans",
+        "Helvetica Neue",
+        "sans-serif",
+      ],
+    },
+    extend: {
+      flexGrow: {
+        2: "2",
+        3: "3",
+      },
+    },
+  },
+};
